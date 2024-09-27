3e34d4d18d4d277cf308f78b05b588994486a1ff
Shibo <shibosoftwaredev@gmail.com>
2024-09-27 10:46:56 +0200

    init desktop program logic

---
+++ app/components/DesktopScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Program from './Program'
+
+const DesktopScreen = () => {
+  return (
+    <section className='absolute w-full h-full top-0 left-0'>
+        <Program/>
+    </section>
+  )
+}
+
+export default DesktopScreen
--- app/desktop/page.tsx
+++ app/desktop/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DesktopBackground from "../components/DesktopBackground";
 import DesktopTaskbar from "../components/DesktopTaskbar";
+import DesktopScreen from "../components/DesktopScreen";
 
 const page = () => {
   return (
     <>
       <DesktopBackground />
       <DesktopTaskbar />
+      <DesktopScreen/>
     </>
   );
 };
