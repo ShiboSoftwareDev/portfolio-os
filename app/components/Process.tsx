f41c4c238ad44ed89b7729652d0473c74190da2f
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2024-10-14 22:15:31 +0200

    process windows are more stable
    no need to resotre old dimensions and remove scrollbars on fullscreen

--- app/components/Process.tsx
+++ app/components/Process.tsx
@@ -6,6 +6,7 @@ import { useProgramStore } from "../programs-metadata/program-store";
 import type { AVAILABLE_PROGRAM_NAMES } from "../programs-metadata/available-programs";
 import { useChangeProgramState } from "../helpers/useChangeProgramState";
 import { Resizable } from "re-resizable";
+import { win32 } from "path";
 
 const Process = ({
   children,
@@ -19,15 +20,18 @@ const Process = ({
   const programState = useProgramStore(
     (state) => state[programName].programState,
   );
-  const [previousX, setPreviousX] = useState(10);
-  const [previousY, setPreviousY] = useState(10);
+  const [previousX, setPreviousX] = useState(0);
+  const [previousY, setPreviousY] = useState(0);
   const changeProgramState = useChangeProgramState(programName);
   const hidden = programState === "minimized" ? "hidden" : "";
   const [size, setSize] = useState("fullscreen");
-  const [width, setWidth] = useState<number | string>(0);
-  const [height, setHeight] = useState<number | string>(0);
+  const [width, setWidth] = useState(500);
+  const [height, setHeight] = useState(500);
+  const fullscreen = size === "fullscreen" ? "h-full w-full" : "";
   return (
     <Draggable
+      key={title}
+      defaultClassName="absolute"
       onStop={(e, data) => {
         if (size === "fullscreen") return;
         setPreviousX(data.lastX);
@@ -37,40 +41,38 @@ const Process = ({
       position={
         size === "fullscreen" ? { x: 0, y: 0 } : { x: previousX, y: previousY }
       }
-      defaultPosition={{ x: 10, y: 10 }}
       handle=".frame"
+      nodeRef={nodeRef}
     >
-      <Resizable
-        className={`border border-blue-600 z-50 absolute ${hidden} bg-white`}
-        size={{
-          width: size !== "fullscreen" ? width : "100%",
-          height: size !== "fullscreen" ? height : "100%",
-        }}
-        onResizeStop={(e, direction, ref, d) => {
-          setWidth(Number(width) + d.width);
-          setHeight(Number(height) + d.height);
-        }}
-      >
-        <div className="frame relative flex flex-row items-center justify-between text-black">
-          <h1>{title}</h1>
-          <div className="flex flex-row gap-2 px-2">
-            <p onMouseDown={() => changeProgramState("minimized")}>-</p>
-            <p
-              onMouseDown={() => {
-                if (size === "fullscreen") {
-                  setHeight(400);
-                  setWidth(400);
-                }
-                setSize(size === "fullscreen" ? "" : "fullscreen");
-              }}
-            >
-              =
-            </p>
-            <p onMouseDown={() => changeProgramState("closed")}>x</p>
+      <div ref={nodeRef} className={fullscreen + " " + hidden}>
+        <Resizable
+          className={`border w-full h-full border-blue-600 z-50 bg-white`}
+          size={{
+            width: size !== "fullscreen" ? width : "100%",
+            height: size !== "fullscreen" ? height : "100%",
+          }}
+          onResizeStop={(e, direction, ref, d) => {
+            setWidth(width + d.width);
+            setHeight(height + d.height);
+          }}
+        >
+          <div className="frame flex flex-row items-center justify-between text-black">
+            <h1>{title}</h1>
+            <div className="flex flex-row gap-2 px-2">
+              <p onMouseDown={() => changeProgramState("minimized")}>-</p>
+              <p
+                onMouseDown={() => {
+                  setSize(size === "fullscreen" ? "" : "fullscreen");
+                }}
+              >
+                =
+              </p>
+              <p onMouseDown={() => changeProgramState("closed")}>x</p>
+            </div>
           </div>
-        </div>
-        {children}
-      </Resizable>
+          {children}
+        </Resizable>
+      </div>
     </Draggable>
   );
 };
