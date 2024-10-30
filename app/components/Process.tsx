2b01bb543279e22fbf8b20b6e6c93082cf8225cd
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2024-10-30 20:50:53 +0200

    desktop: resize from all directions part 2

--- app/components/Process.tsx
+++ app/components/Process.tsx
@@ -22,8 +22,6 @@ const Process = ({
   const updateLastActive = useProgramStore(
     (state) => state[programName].updateLastActive,
   );
-  const [previousX, setPreviousX] = useState(0);
-  const [previousY, setPreviousY] = useState(0);
   const changeProgramState = useChangeProgramState(programName);
   const hidden = programState === "minimized" ? "hidden" : "";
   const [size, setSize] = useState("fullscreen");
@@ -31,73 +29,57 @@ const Process = ({
   const [height, setHeight] = useState(500);
   const fullscreen = size === "fullscreen" ? "h-full w-full" : "";
   return (
-    <Draggable
-      key={title}
-      defaultClassName="absolute"
-      onStop={(e, data) => {
-        if (size === "fullscreen") return;
-        setPreviousX(data.lastX);
-        setPreviousY(data.lastY);
-      }}
-      bounds="parent"
-      position={
-        size === "fullscreen" ? { x: 0, y: 0 } : { x: previousX, y: previousY }
-      }
-      handle=".frame"
-      nodeRef={nodeRef}
+    <div
+      onMouseDownCapture={updateLastActive}
+      ref={nodeRef}
+      className={fullscreen + " " + hidden}
     >
-      <div
-        onMouseDownCapture={updateLastActive}
-        ref={nodeRef}
-        className={fullscreen + " " + hidden}
+      <Resizable
+        dragHandleClassName="frame"
+        className={`border w-full h-full border-blue-600 z-50 bg-white`}
+        size={{
+          width: size !== "fullscreen" ? width : "100%",
+          height: size !== "fullscreen" ? height : "100%",
+        }}
+        onResizeStop={(e, direction, ref, d) => {
+          setWidth(width + d.width);
+          setHeight(height + d.height);
+        }}
       >
-        <Resizable
-          className={`border w-full h-full border-blue-600 z-50 bg-white`}
-          disableDragging
-          size={{
-            width: size !== "fullscreen" ? width : "100%",
-            height: size !== "fullscreen" ? height : "100%",
-          }}
-          onResizeStop={(e, direction, ref, d) => {
-            setWidth(width + d.width);
-            setHeight(height + d.height);
-          }}
-        >
-          <div className="frame h-8 flex flex-row border-b border-blue-300 items-center justify-between text-black">
-            <h1>{title}</h1>
-            <div className="flex flex-row h-full">
-              <button className="h-full aspect-square border-l border-blue-300">
-                <p
-                  className="h-full aspect-square flex items-center justify-center"
-                  onMouseDown={() => changeProgramState("minimized")}
-                >
-                  -
-                </p>
-              </button>
-              <button className="h-full aspect-square border-l border-blue-300">
-                <p
-                  className="h-full aspect-square flex items-center justify-center"
-                  onMouseDown={() => {
-                    setSize(size === "fullscreen" ? "" : "fullscreen");
-                  }}
-                >
-                  =
-                </p>
-              </button>
-              <button className="h-full aspect-square border-l border-red-500 hover:bg-red-500">
-                <p
-                  className="h-full aspect-square flex items-center justify-center"
-                  onMouseDown={() => changeProgramState("closed")}
-                >
-                  x
-                </p>
-              </button>
-            </div>
+        <div className="frame h-8 flex flex-row border-b border-blue-300 items-center justify-between text-black">
+          <h1>{title}</h1>
+          <div className="flex flex-row h-full">
+            <button className="h-full aspect-square border-l border-blue-300">
+              <p
+                className="h-full aspect-square flex items-center justify-center"
+                onMouseDown={() => changeProgramState("minimized")}
+              >
+                -
+              </p>
+            </button>
+            <button className="h-full aspect-square border-l border-blue-300">
+              <p
+                className="h-full aspect-square flex items-center justify-center"
+                onMouseDown={() => {
+                  setSize(size === "fullscreen" ? "" : "fullscreen");
+                }}
+              >
+                =
+              </p>
+            </button>
+            <button className="h-full aspect-square border-l border-red-500 hover:bg-red-500">
+              <p
+                className="h-full aspect-square flex items-center justify-center"
+                onMouseDown={() => changeProgramState("closed")}
+              >
+                x
+              </p>
+            </button>
           </div>
-          {children}
-        </Resizable>
-      </div>
-    </Draggable>
+        </div>
+        {children}
+      </Resizable>
+    </div>
   );
 };
 
