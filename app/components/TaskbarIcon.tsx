021a12fc758b308ba51716365becb035ecbb315c
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2024-11-22 15:04:20 +0200

    desktop: taskbar icons tooltip

--- app/components/TaskbarIcon.tsx
+++ app/components/TaskbarIcon.tsx
@@ -3,6 +3,7 @@
 import { useChangeProgramState } from "../helpers/useChangeProgramState";
 import type { AVAILABLE_PROGRAM_NAMES } from "../programs-metadata/available-programs";
 import { useProgramStore } from "../programs-metadata/program-store";
+import { Tooltip } from "react-tooltip";
 
 const TaskbarIcon = ({
   programName,
@@ -17,6 +18,8 @@ const TaskbarIcon = ({
     (state) => state[programName].updateLastActive,
   );
   const icon = useProgramStore((state) => state[programName].icon);
+  const title = useProgramStore((state) => state[programName].title);
+
   const handleClick = () => {
     updateLastActive();
     changeProgramState(programState !== "open" ? "open" : "minimized");
@@ -28,13 +31,17 @@ const TaskbarIcon = ({
         ? "border-blue-500 bg-white"
         : "border-white bg-white";
   return (
-    <div
-      onClick={handleClick}
-      onKeyDown={() => null}
-      className={`h-[80%] aspect-square flex justify-center items-center text-black text-center box-border rounded-[25%] border-2 ${borderColor} select-none`}
-    >
-      {icon}
-    </div>
+    <>
+      <div
+        data-tooltip-id={title}
+        onClick={handleClick}
+        onKeyDown={() => null}
+        className={`h-[80%] aspect-square flex justify-center items-center text-black text-center box-border rounded-[25%] border-2 ${borderColor} select-none`}
+      >
+        {icon}
+      </div>
+      <Tooltip id={title} place="top" content={title} />
+    </>
   );
 };
 export default TaskbarIcon;
